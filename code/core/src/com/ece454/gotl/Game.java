9c5211f08e6afa14ad389be7845dbad600ebd0ae
Thomas Polkowski <tppolkow@uwaterloo.ca>
2019-06-17 16:13:54 -0400

    goose image

--- code/core/src/com/ece454/gotl/Game.java
+++ code/core/src/com/ece454/gotl/Game.java
@@ -3,31 +3,83 @@ package com.ece454.gotl;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	static final float PIXEL_PER_METER = 32f;
+
+	private static final float SCALE = 2.0f;
+	private static final float TIME_STEP = 1 / 60f;
+	private static final int VELOCITY_ITERATIONS = 6;
+	private static final int POSITION_ITERATIONS = 2;
+	private SpriteBatch batch;
+	private Texture img;
+	private Box2DDebugRenderer box2DDebugRenderer;
+	private World world;
+	private Goose goose;
+	private static final float VELOCITY_Y = -9.85f;
+	private static final float VELOCITY_X = 0f;
+	private OrthographicCamera orthographicCamera;
+
 	@Override
 	public void create () {
+		orthographicCamera = new OrthographicCamera();
+		orthographicCamera.setToOrtho(false, Gdx.graphics.getWidth() / SCALE, Gdx.graphics.getHeight() / SCALE);
+		world = new World(new Vector2(VELOCITY_X, VELOCITY_Y), false);
+		goose = new Goose(world);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = goose.texture;
+		box2DDebugRenderer = new Box2DDebugRenderer();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		update();
+		Gdx.gl.glClearColor(0.5f, 0.8f, 1f, 1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+//		System.out.println("y " + goose.xPositionInTexture);
+//		System.out.println("x " + goose.yPositionInTexture);
+//		System.out.println("width " + goose.widthInTexture);
+//		System.out.println("height " + goose.heightInTexture);
+
+		batch.draw(img, goose.getBody().getPosition().x * PIXEL_PER_METER - (img.getWidth() / 2f),
+				goose.getBody().getPosition().y * PIXEL_PER_METER - (img.getHeight() / 2f), goose.xPositionInTexture, goose.yPositionInTexture, goose.widthInTexture, goose.heightInTexture);
 		batch.end();
+
+		box2DDebugRenderer.render(world, orthographicCamera.combined.scl(PIXEL_PER_METER));
+	}
+
+	private void update(){
+		world.step(TIME_STEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+		cameraUpdate();
+		batch.setProjectionMatrix(orthographicCamera.combined);
+	}
+
+	private void cameraUpdate(){
+		Vector3 position = orthographicCamera.position;
+		position.x = goose.getBody().getPosition().x * PIXEL_PER_METER;
+		position.y = goose.getBody().getPosition().y * PIXEL_PER_METER;
+		orthographicCamera.position.set(position);
+		orthographicCamera.update();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		box2DDebugRenderer.dispose();
+		world.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height){
+		orthographicCamera.setToOrtho(false, width/SCALE, height/SCALE);
 	}
 }
---
+++ code/core/src/com/ece454/gotl/Goose.java
@@ -0,0 +1,53 @@
+package com.ece454.gotl;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Goose {
+    public static final String PLAYER_IMG_PATH = "goose.png";
+    public Texture texture = new Texture(Goose.PLAYER_IMG_PATH);
+//    public int widthInTexture = texture.getWidth()/columns;
+    public int widthInTexture = 60;
+//    public int heightInTexture = 60;
+    public int heightInTexture = texture.getHeight()/rows;
+//    public int heightInTexture = 50;
+    public int xPositionInTexture = texture.getWidth()/columns - widthInTexture;
+    public int yPositionInTexture = texture.getHeight()/rows;
+
+
+    private static final int rows = 10;
+    private static final int columns = 8;
+    private static final int BOX_SIZE = 32;
+    private static final float PLAYER_DENSITY = 1.0f;
+    public static final float JUMP_FORCE = 250f;
+    public static final float RUN_FORCE = 5f;
+    private static final float PLAYER_START_X = 28f;
+    private static final float PLAYER_START_Y = 18f;
+    private Body body;
+
+    public Goose(World world) {
+        createBoxBody(world, PLAYER_START_X, PLAYER_START_Y);
+    }
+
+    private void createBoxBody(World world, float x, float y) {
+        BodyDef bdef = new BodyDef();
+        bdef.fixedRotation = true;
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        bdef.position.set(x, y);
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(BOX_SIZE / Game.PIXEL_PER_METER / 2, BOX_SIZE / Game.PIXEL_PER_METER / 2);
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = PLAYER_DENSITY;
+        body = world.createBody(bdef);
+        body.createFixture(fixtureDef).setUserData(this);
+    }
+
+    public Body getBody() {
+        return body;
+    }
+}
