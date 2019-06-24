2767923df6b4fa23ba830882e8ea926c19d7f208
AnandMathew <a9mathew@edu.uwaterloo.ca>
2019-06-23 22:10:16 -0400

    menu screen font and skin upgrades for teh demo

--- code/core/src/screens/MenuScreen.java
+++ code/core/src/screens/MenuScreen.java
@@ -26,7 +26,8 @@ import states.PlayState;
 public class MenuScreen extends ScreenAdapter {
 
     private static final String START = "START";
-    private static final String TITLEOFGAME = "GOOSE ON THE LOOSE";
+    private static final String TITLEOFGAMEP1 = "GOOSE";
+    private static final String TITLEOFGAMEP2 = "ON THE LOOSE";
     private GotlGame game;
     private Stage stage;
     private AssetHandler assetHandler;
@@ -64,10 +65,20 @@ public class MenuScreen extends ScreenAdapter {
     }
 
     private void createButton() {
-        Skin skin = assetHandler.manager.get(assetHandler.BTN_SKIN_PATH, Skin.class);
-        Button startBtn = new TextButton(START, skin);
+
+        // For now removing button skin
+        // Skin skin = assetHandler.manager.get(assetHandler.BTN_SKIN_PATH, Skin.class);
+        // skin.get(TextButton.TextButtonStyle.class).font = assetHandler.manager.get(assetHandler.FONT_PATH, BitmapFont.class);
+
+        TextButton.TextButtonStyle btnStyle = new TextButton.TextButtonStyle();
+        btnStyle.font= assetHandler.manager.get(assetHandler.FONT_PATH, BitmapFont.class);
+        Button startBtn = new TextButton(START, btnStyle);
+
         startBtn.setWidth(Gdx.graphics.getWidth() / 2);
-        startBtn.setPosition(Gdx.graphics.getWidth() / 2.6f,Gdx.graphics.getHeight() / 2, Align.center);
+        startBtn.setPosition(Gdx.graphics.getWidth() / 2.6f,Gdx.graphics.getHeight() / 8, Align.center);
+
+        startBtn.setSize(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/4);
+        // startBtn.setFontScale(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/4);
         startBtn.setTransform(true);
         startBtn.scaleBy(0.5f);
         startBtn.addListener(new ChangeListener() {
@@ -80,14 +91,23 @@ public class MenuScreen extends ScreenAdapter {
     }
 
     private void createLabel() {
-        int row_height = Gdx.graphics.getWidth() / 12;
+        int row_height = Gdx.graphics.getWidth() / 5;
         Label.LabelStyle labelStyle = new Label.LabelStyle();
         labelStyle.font = assetHandler.manager.get(assetHandler.FONT_PATH, BitmapFont.class);
-        Label title = new Label(TITLEOFGAME, labelStyle);
-        title.setSize(Gdx.graphics.getWidth(), row_height);
-        title.setAlignment(Align.center);
-        title.setPosition(0,Gdx.graphics.getHeight()-row_height*2);
-        title.setWidth(Gdx.graphics.getWidth());
-        stage.addActor(title);
+        Label titlep1 = new Label(TITLEOFGAMEP1, labelStyle);
+        titlep1.setSize(Gdx.graphics.getWidth(), row_height);
+        titlep1.setAlignment(Align.center);
+        titlep1.setPosition(0,Gdx.graphics.getHeight()-row_height*2);
+        titlep1.setWidth(Gdx.graphics.getWidth());
+        titlep1.setFontScale(2f);
+
+        Label titlep2 = new Label(TITLEOFGAMEP2, labelStyle);
+        titlep2.setSize(Gdx.graphics.getWidth(), row_height);
+        titlep2.setAlignment(Align.center);
+        titlep2.setPosition(0,Gdx.graphics.getHeight()-row_height*3);
+        titlep2.setWidth(Gdx.graphics.getWidth());
+        titlep2.setFontScale(2f);
+        stage.addActor(titlep1);
+        stage.addActor(titlep2);
     }
 }
