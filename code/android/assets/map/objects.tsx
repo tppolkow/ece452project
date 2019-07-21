b76be6f7b3bf19f9259d1bb2368cd893f4c6764b
Howard Tsai <cttsai@edu.uwaterloo.ca>
2019-07-20 21:18:06 -0400

    Fix screen ratio

--- code/android/assets/map/objects.tsx
+++ code/android/assets/map/objects.tsx
@@ -1,14 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="objects" tilewidth="325" tileheight="260" tilecount="18" columns="0">
+<tileset version="1.2" tiledversion="1.2.4" name="objects" tilewidth="325" tileheight="260" tilecount="16" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="313" height="260" source="../objects/Tree.png"/>
- </tile>
- <tile id="1">
-  <image width="101" height="99" source="../objects/StoneBlock.png"/>
- </tile>
- <tile id="2">
-  <image width="124" height="73" source="../objects/Stone.png"/>
  </tile>
  <tile id="3">
   <image width="150" height="51" source="../objects/Skeleton.png"/>
@@ -19,20 +13,11 @@
  <tile id="5">
   <image width="85" height="88" source="../objects/Sign.png"/>
  </tile>
- <tile id="6">
-  <image width="102" height="50" source="../objects/Grass (2).png"/>
- </tile>
- <tile id="7">
-  <image width="102" height="50" source="../objects/Grass (1).png"/>
- </tile>
  <tile id="8">
   <image width="101" height="101" source="../objects/Crate.png"/>
  </tile>
  <tile id="9">
   <image width="86" height="96" source="../objects/Cactus (3).png"/>
- </tile>
- <tile id="10">
-  <image width="70" height="45" source="../objects/Cactus (2).png"/>
  </tile>
  <tile id="11">
   <image width="108" height="111" source="../objects/Cactus (1).png"/>
@@ -43,16 +28,25 @@
  <tile id="13">
   <image width="145" height="88" source="../objects/Bush (1).png"/>
  </tile>
- <tile id="17">
+ <tile id="21">
+  <image width="70" height="45" source="../objects/Cactus (2).png"/>
+ </tile>
+ <tile id="22">
   <image width="325" height="135" source="../objects/cloud.png"/>
  </tile>
- <tile id="18">
+ <tile id="23">
   <image width="125" height="102" source="../objects/danger_1.png"/>
  </tile>
- <tile id="19">
+ <tile id="24">
   <image width="64" height="64" source="../objects/danger_2.png"/>
  </tile>
- <tile id="20">
+ <tile id="25">
   <image width="32" height="32" source="../objects/danger_3.png"/>
+ </tile>
+ <tile id="26">
+  <image width="124" height="73" source="../objects/Stone.png"/>
+ </tile>
+ <tile id="27">
+  <image width="101" height="99" source="../objects/StoneBlock.png"/>
  </tile>
 </tileset>
