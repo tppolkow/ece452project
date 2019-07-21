b611e01cfb57eb043de5c66597a24705780d2ae5
Howard Tsai <cttsai@edu.uwaterloo.ca>
2019-07-21 13:56:03 -0400

    New menu screen

--- code/core/src/states/MenuState.java
+++ code/core/src/states/MenuState.java
@@ -2,36 +2,41 @@ package states;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.RepeatAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.ece454.gotl.GotlGame;
 
 import handlers.AssetHandler;
 import handlers.GameStateManager;
 
 public class MenuState extends State {
 
-    private static final String START = "START";
-    private static final String TITLEOFGAMEP1 = "GOOSE";
-    private static final String TITLEOFGAMEP2 = "ON THE LOOSE";
     private Stage stage;
     private AssetHandler assetHandler;
 
-    // needs a lot more design
     public MenuState(GameStateManager gsm) {
         super(gsm);
         assetHandler = gsm.getGame().getAssetHandler();
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
-        createLabel();
+        createCrest();
         createButton();
     }
 
@@ -59,22 +64,10 @@ public class MenuState extends State {
     }
 
     private void createButton() {
-
-
-        // For now removing button skin
-        // Skin skin = assetHandler.manager.get(assetHandler.BTN_SKIN_PATH, Skin.class);
-        // skin.get(TextButton.TextButtonStyle.class).font = assetHandler.manager.get(assetHandler.FONT_PATH, BitmapFont.class);
-
-        TextButton.TextButtonStyle btnStyle = new TextButton.TextButtonStyle();
-        btnStyle.font= assetHandler.getManager().get(assetHandler.FONT_PATH, BitmapFont.class);
-        Button startBtn = new TextButton(START, btnStyle);
-        startBtn.setWidth(Gdx.graphics.getWidth() / 2);
-        startBtn.setPosition(Gdx.graphics.getWidth() / 2.6f,Gdx.graphics.getHeight() / 8, Align.center);
-
-        startBtn.setSize(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/4);
-        // startBtn.setFontScale(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/4);
-        startBtn.setTransform(true);
-        startBtn.scaleBy(0.5f);
+        Texture texture = assetHandler.getManager().get(assetHandler.START_BUTTON_PATH, Texture.class);
+        Drawable drawable = new TextureRegionDrawable(new TextureRegion(texture));
+        Button startBtn = new ImageButton(drawable);
+        startBtn.setPosition(Gdx.graphics.getWidth() / 1.95f,Gdx.graphics.getHeight() / 5, Align.center);
         startBtn.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
@@ -86,24 +79,16 @@ public class MenuState extends State {
         stage.addActor(startBtn);
     }
 
-    private void createLabel() {
-        int row_height = Gdx.graphics.getWidth() / 5;
-        Label.LabelStyle labelStyle = new Label.LabelStyle();
-        labelStyle.font = assetHandler.getManager().get(assetHandler.FONT_PATH, BitmapFont.class);
-        Label titlep1 = new Label(TITLEOFGAMEP1, labelStyle);
-        titlep1.setSize(Gdx.graphics.getWidth(), row_height);
-        titlep1.setAlignment(Align.center);
-        titlep1.setPosition(0,Gdx.graphics.getHeight()-row_height*2);
-        titlep1.setWidth(Gdx.graphics.getWidth());
-        titlep1.setFontScale(1.4f);
-
-        Label titlep2 = new Label(TITLEOFGAMEP2, labelStyle);
-        titlep2.setSize(Gdx.graphics.getWidth(), row_height);
-        titlep2.setAlignment(Align.center);
-        titlep2.setPosition(0,Gdx.graphics.getHeight()-row_height*3);
-        titlep2.setWidth(Gdx.graphics.getWidth());
-        titlep2.setFontScale(1.4f);
-        stage.addActor(titlep1);
-        stage.addActor(titlep2);
+    private void createCrest() {
+        Image crest = new Image(assetHandler.getManager().get(assetHandler.GOTL_CREST, Texture.class));
+        Image goose = new Image(assetHandler.getManager().get(assetHandler.GOOSE_FACE_PATH, Texture.class));
+        crest.addAction(Actions.repeat(RepeatAction.FOREVER, Actions.rotateBy(20, 1)));
+        crest.setPosition(Gdx.graphics.getWidth() / 2.02f - crest.getWidth() / 2, Gdx.graphics.getHeight() / 1.5f - crest.getWidth() / 2);
+        crest.setOrigin(crest.getWidth() / 2, crest.getHeight() / 2);
+        crest.setAlign(Align.center);
+        goose.setOrigin(crest.getWidth() / 2, crest.getHeight() / 2);
+        goose.setPosition(Gdx.graphics.getWidth() / 2.25f - goose.getWidth() / 2, Gdx.graphics.getHeight() / 1.56f - goose.getWidth() / 2);
+        stage.addActor(crest);
+        stage.addActor(goose);
     }
 }
